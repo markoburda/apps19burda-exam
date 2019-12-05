217ca8f8e4539e86fdedfa8e0aaf51b11c79b59b
markoburda <burda@ucu.edu.ua>
2019-12-05 14:44:56 +0200

    Concede

---
+++ src/main/java/json/JsonObject.java
@@ -0,0 +1,41 @@
+package json;
+
+//import sun.awt.image.ImageWatched;
+
+import java.util.*;
+
+/**
+ * Created by Andrii_Rodionov on 1/3/2017.
+ */
+public class JsonObject extends Json {
+    private ArrayList jArray = new ArrayList();
+
+    public JsonObject(JsonPair... jsonPairs) {
+        for(JsonPair jPair : jsonPairs){
+            jArray.add(jPair);
+        }
+    }
+
+    @Override
+    public String toJson() {
+        String jStr = "";
+       for(Object jPair : jArray){
+           jStr += ((JsonPair) jPair).key + ":" + ((JsonPair) jPair).value;
+       }
+       return jStr.toString();
+    }
+
+    public void add(JsonPair jsonPair) {
+        // ToDo
+    }
+
+    public Json find(String name) {
+        // ToDo
+        return null;
+    }
+
+    public JsonObject projection(String... names) {
+        // ToDo
+        return null;
+    }
+}
